991493de29c6ee0836c0941bffd3c79be0f22866
robertramosastudillo <robertramosastudillo@gmail.com>
2020-06-29 16:27:06 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlBD;
 if(process.env.NODE_ENV === 'dev'){
     urlBD = "mongodb://localhost:27017/cafe";
 }else{
-    urlBD = "mongodb+srv://robertramosastudillo:SRl8OnL5XN0R1Qal@cluster0-ggagu.mongodb.net/cafe";
+    urlBD = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlBD;
