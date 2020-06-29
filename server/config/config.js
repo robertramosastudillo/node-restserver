7a403374322b9a080573757266fa1c4b680ff162
robertramosastudillo <robertramosastudillo@gmail.com>
2020-06-29 15:14:45 -0500

    RestServer - usario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,22 @@
 // Puerto
 // ===============
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ================
+// Entorno
+// ===============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// ================
+// Base de datos
+// ===============
+let urlBD;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlBD = "mongodb://localhost:27017/cafe";
+}else{
+    urlBD = "mongodb+srv://robertramosastudillo:SRl8OnL5XN0R1Qal@cluster0-ggagu.mongodb.net/cafe";
+}
+
+process.env.URLDB = urlBD;
